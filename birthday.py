9d3b7101b616fde8cf1325aefbcd6a4f4c5e8fa8
lolabug11 <139627294+lolabug11@users.noreply.github.com>
2025-04-10 22:09:15 +0000

    gave up on minygame

--- birthday.py
+++ birthday.py
@@ -22,64 +22,20 @@ while repeat == 'yes':
         t.sleep(0.5)
         print("Happy Birthday to youuuuuuuu!!!!")
         t.sleep(0.5)
-        higherguees = int(input('Do you want to play a game of higher or lower or do you just want me to guess(1 for minygame): '))
-        if higherguees == 1:
-            if age > 30:
-                guess = age - 20
-            elif age < 10:
-                guess = age - 1
-            else:
-                guess = age - 6
-            print('Are you ' + str(guess) + '?')
-            higherorlower = input('Am i to high or to low or am i correct: ')
-            higherorlower = higherorlower.lower()
-            if 'h' in higherorlower:
-                guess = guess - 5
-                print('Are you ' + str(guess) + '?')
-                higherorlower= input('Was I on point a little high or a little low: ')
-                if 'h' in higherorlower:
-                    guess = guess - 5
-                    print('Are you ' + str(guess) + '?')
-                    higherorlower= input('Was I on point a little high or a little low: ')
-                elif 'l':
-                    guess = guess + 5
-            elif 'l' in higherorlower:
-                guess = guess + 5
-                print('Are you ' + str(guess) + '?')
-                higherorlower = input('Am i to high or to low or am i correct: ')     
-                if 'h' in higherorlower:
-                    guess = guess - 5
-                    print('Are you ' + str(guess) + '?')
-                    higherorlower= input('Was I on point a little high or a little low: ')
-                elif 'l' in higherorlower:
-                    guess = guess + 5
-                    print('Are you ' + str(guess) + '?') 
-                    higherorlower= input('Was I on point a little high or a little low: ')
-                    if 'h' in higherorlower:
-                        guess = guess - 5
-                        print('Are you ' + str(guess) + '?')
-                        higherorlower = input('Was I on point a little high or a little low: ')
-                        print('My final guess is ' + str(age))
-                    elif 'l' in higherorlower:
-                        guess = guess + 5
-                        print('Are you ' + str(guess) + '?')
-                        higherorlower= input('Was I on point a little high or a little low: ')
-                        print('My final guess is ' + str(age))
+        guess = 1
+        while guess < age + 1:
+            print('Are you ' + str(guess))
+            guess = guess + 1
+            t.sleep(0.5)
+        print('*You say* STOP')
+
+        if guess > 60:
+            print('Wow Your old')
+
+        elif guess > 116:
+            print("You are tied for the world recod of oldedt person alive!")
         else:
-            guess = 1
-            while guess < age + 1:
-                print('Are you ' + str(guess))
-                guess = guess + 1
-                t.sleep(0.5)
-            print('*You say* STOP')
-
-            if guess > 60:
-                print('Wow Your old')
-
-            elif guess > 116:
-                print("You are tied for the world recod of oldedt person alive!")
-            else:
-                print("Have a good birthdy")
+            print("Have a good birthdy")
         repeat = input('do you want to repeat:')
     else:
         print('Have a good day, ' + name)

00273fe69d8b187232557d1f2cb973fb140d165e
lolabug11 <139627294+lolabug11@users.noreply.github.com>
2025-04-14 14:57:39 -0400

    Create sigmaboy(request).py

---
+++ sigmaboy(request).py
@@ -0,0 +1,31 @@
+print('Sigma-sigma boy, sigma boy, sigma boy
+Каждая девчонка хочет танцевать с тобой
+Sigma-sigma boy, sigma boy, sigma boy
+Я такая вся, что добиваться будешь год
+Sigma-sigma boy, sigma boy, sigma boy
+Каждая девчонка хочет танцевать с тобой
+Sigma-sigma boy, sigma boy, sigma boy
+Я такая вся, что добиваться будешь год
+Sigma-sigma boy, sigma boy, sigma boy
+Каждая девчонка хочет танцевать с тобой
+Sigma-sigma boy, sigma boy, sigma boy
+Я такая вся, что добиваться будешь год
+Так что милый мальчик будет прыгать и сгорать (ага)
+Ой, пакуйся в мою Bentley, полезай в мой beauty box (давай)
+Ты мой sigma boy, но я желаю твоих слёз (понял?)
+Sigma-sigma boy, бэк на стол, достаю bank roll
+Я куплю все Skittles, Snickers, точно будешь мой, boy
+Sigma-sigma boy, я, икона твоих икон
+Обо мне мечтают так, как будто я биткоин (понял?)
+Sigma-sigma бой, но не мой, молча, как немой
+Смотрит в никуда, ой какой, этот взгляд, огонь
+Годы будут ждать, когда ты соберёшься
+И меня когда-нибудь добьёшься
+Sigma-sigma boy, sigma boy, sigma boy
+Каждая девчонка хочет танцевать с тобой
+Sigma-sigma boy, sigma boy, sigma boy
+Я такая вся, что добиваться будешь год
+Sigma-sigma boy, sigma boy, sigma boy
+Каждая девчонка хочет танцевать с тобой
+Sigma-sigma boy, sigma boy, sigma boy
+Я такая вся, что добиваться будешь год"

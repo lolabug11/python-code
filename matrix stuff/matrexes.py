eed5f222c249f010f14713a79a94c8c4f6f8a961
Lola Bug <139627294+lolabug11@users.noreply.github.com>
2026-03-06 00:49:52 +0000

    refactor Matrex class to remove name attribute and update row handling

--- matrix stuff/matrexes.py
+++ matrix stuff/matrexes.py
@@ -1,15 +1,21 @@
+import random as r
 class Matrex:
-    def __init__(self, data:list, width, length, name:str):
+    def __init__(self, data:list, width, length):
         self.data = data
         self.width = width
         self.length = length
-        self.name = name
-
-        rows = {}
+        self.id = r.randint(1,1000)
+        rows = []
         for x in range(self.length):
             start = x * self.width
             end = start + self.width
-            rows[f'row{x+1}'] = self.data[start:end]
+            rows.append(self.data[start:end])
+            rows.append('\n')
             self.rows = rows
-test = Matrex([1,2,3,4,5,6,7,8,9], 3,3, 'Test')
-print(f'{test.name} data is {test.rows}')
+        row_data = ''
+        for x in self.rows:
+            row_data += str(x)
+        self.rows = row_data
+
+test = Matrex([1,2,3,4,5,6,7,8,9], 3,3)
+print(f'The matrix with the id of {test.id} has data that is \n{test.rows}')

d2294bff087738a652427fc242d922992dd95233
Lola Bug <139627294+lolabug11@users.noreply.github.com>
2026-03-17 22:11:27 +0000

    Add graph testing functionality and test cases for bipartiteness

--- graph_therory/graphTheoryClass.py
+++ graph_therory/graphTheoryClass.py
@@ -1,13 +1,28 @@
 class Graph:
-    def __init__(self):
+    def __init__(self,weighted = False, directed = False):
         self.vertices = []
         self.edges = []
         self.adjacency_list = {}
+        self.weighted = weighted
+        self.directed = directed
     
 
+
+    def __str__(self):
+        return_string = ''
+        return_string += f'Vertices:\n{self.vertices}\nEdges:\n{self.edges}\nAdjacencys:\n'
+        for item in self.adjacency_list:
+            return_string += f'{item} -> '
+            return_string += f'{self.adjacency_list[item]}\n'
+        return return_string
+
+
+
     def add_vertex(self, vertex):
-        self.vertices.append(vertex)
-        self.adjacency_list[vertex] = []
+        if vertex not in self.vertices:
+            self.vertices.append(vertex)
+            self.adjacency_list[vertex] = []
+        
     
 
 
@@ -18,29 +33,221 @@ class Graph:
         if v2 not in self.vertices:
             self.vertices.append(v2)
             self.adjacency_list[v2] = []
-        self.adjacency_list[v1].append(v2)
-        self.adjacency_list[v2].append(v1)
+        if self.directed:
+            self.adjacency_list[v1].append(v2)
+        else:
+            self.adjacency_list[v1].append(v2)
+            self.adjacency_list[v2].append(v1)
         self.edges.append((v1,v2,weight))
 
 
 
     def remove_vertex(self,vertex):
-        if vertex not in self.vertices:
-            raise TypeError(f'{vertex} is not in this graph')
-            
-        
+        self._validate_vertex(vertex)
         self.vertices.remove(vertex)
         del self.adjacency_list[vertex]
         for item in self.adjacency_list:
             if vertex in self.adjacency_list[item]:
                 self.adjacency_list[item].remove(vertex)
+        for edge in self.edges:
+            if vertex in edge:
+                self.edges.remove(edge)
 
     
 
-    def remove_edge(self,v1,v2,weight):
-        if v2 in self.adjacency_list[v1]:
-            adjace
-        if (v1,v2,weight) in self.edges:
-            self.edges.remove((v1,v2,weight))
-        elif (v2,v1,weight) in self.edges:
-            self.edges.remove((v2,v1,weight))
+    def remove_edge(self,v1,v2,weight=None):
+        self._validate_vertex(v1)
+        self._validate_vertex(v2)
+        if self.directed:
+            if v2 in self.adjacency_list[v1]:
+                self.adjacency_list[v1].remove(v2)
+            if (v1,v2,weight) in self.edges:
+                self.edges.remove(v1,v2,weight)
+        else:
+            if v2 in self.adjacency_list[v1]:
+                self.adjacency_list[v1].remove(v2)
+            if v1 in self.adjacency_list[v2]:
+                self.adjacency_list[v2].remove(v1)
+            if (v1,v2,weight) in self.edges:
+                self.edges.remove((v1,v2,weight))
+            elif (v2,v1,weight) in self.edges:
+                self.edges.remove((v2,v1,weight))
+    
+
+
+    def vertex_exists(self,vertex):
+        return vertex in self.vertices
+
+
+
+    def  edge_exists(self, v1,v2):
+        for edge in self.edges:
+            if v1 in edge and v2 in edge:
+                return True
+        return False
+    
+
+
+    def _validate_vertex(self,vertex):
+        if vertex not in self.vertices:
+            raise TypeError(f'{vertex} is not in {self}')
+    
+
+
+    def degree(self, vertex):
+        self._validate_vertex(vertex)
+        return len(self.adjacency_list[vertex])
+    
+
+
+    def degree_sequence(self):
+        degree_sequence = []
+        for vertex in self.adjacency_list:
+            degree_sequence.append(len(self.adjacency_list[vertex]))
+        degree_sequence.sort(reverse=True)
+        return degree_sequence
+
+
+
+    def average_degree(self):
+        average = 0 
+        sequence = self.degree_sequence()
+        for x in sequence:
+            average += x
+        average /= len(sequence)
+        return average
+    
+
+
+    def max_possible_edges(self):
+        n = len(self.vertices)
+        return n*(n-1)/2
+    
+
+
+    def edge_count(self):
+        return len(self.edges)
+
+
+
+    def vertex_count(self):
+        return len(self.vertices)
+
+
+    def is_complete(self):
+        return self.edge_count() == self.max_possible_edges()
+    
+
+
+    def BFS(self, start):
+        if start in self.vertices:
+            visited = []
+            to_visit = [start]
+            
+            while len(to_visit) > 0:
+                current = to_visit[0]
+                to_visit.remove(to_visit[0])
+                visited.append(current)
+                for neighbor in self.adjacency_list[current]:
+                    if neighbor not in visited and neighbor not in to_visit:
+                        to_visit.append(neighbor)
+
+            return visited   
+        else:
+            raise TypeError(f'{start} is not a real vertex')   
+    
+
+
+    @staticmethod
+    def DFS_DFS(graph,visited, start):
+        visited.append(start)
+        for neighbor in graph.adjacency_list[start]:
+            if neighbor not in visited:
+                Graph.DFS_DFS(graph,visited,neighbor)
+
+    def DFS(self, start):
+        visited = []
+        Graph.DFS_DFS(self,visited,start)
+        return visited
+    
+
+
+    def has_cycle(self):
+        visited = []
+        for x in self.vertices:
+            if x not in visited:
+                a = Graph.cycle_DFS(self,visited,x)
+                if a :
+                    return True
+        
+        return False
+
+    @staticmethod
+    def cycle_DFS(graph,visited,node,parent = None):
+        visited.append(node)
+        for neighbor in graph.adjacency_list[node]:
+            if neighbor not in visited:
+                if Graph.cycle_DFS(graph,visited,neighbor, node):
+                    return True
+            else:
+                if neighbor != parent:
+                    return True
+        return False
+    
+
+
+    def is_bipartite(self):
+        color = {}
+        for vertex in self.vertices:
+            if vertex not in color:
+                color[vertex] = 0
+                if Graph.traversal(self,color,vertex) == False:
+                    return False
+                
+                return True
+
+    
+
+
+    @staticmethod
+    def traversal(graph,color,vertex):
+        for neighbor in graph.adjacency_list[vertex]:
+            if neighbor not in color:
+                if color[vertex] == 0:
+                    color[neighbor] = 1
+                    if Graph.traversal(graph,color,neighbor) == False:
+                        return False
+                else:
+                    color[neighbor] = 0
+                    if Graph.traversal(graph,color,neighbor) == False:
+                        return False
+            else:
+                if color[neighbor] == color[vertex]:
+                    return False
+        return True
+            
+
+
+    def is_connected(self):
+        if len(self.vertices) != 0:
+            return len(self.BFS(self.vertices[0])) == self.vertex_count()
+        else:
+            return True
+    
+
+
+    def get_neighbors(self,vertex):
+        return self.adjacency_list[vertex]
+    
+
+
+    def density(self):
+        return self.edge_count() / self.max_possible_edges()
+    
+
+
+    def is_tree(self):
+        return self.is_connected() and self.edge_count() == self.vertex_count() -1
+    
+
+
---
+++ graph_therory/graph_test.py
@@ -0,0 +1,30 @@
+from graphTheoryClass import *
+import json
+
+with open('graph_therory/tests.json', "r") as f:
+    tests = json.load(f)
+
+passed = 0
+
+for test in tests:
+    g = Graph()
+
+    # add vertices
+    for v in test["vertices"]:
+        g.add_vertex(v)
+
+    # add edges
+    for v1, v2 in test["edges"]:
+        g.add_edge(v1, v2)
+
+    # run test
+    result = g.is_bipartite()
+    expected = test["expected"]
+
+    if result == expected:
+        print(test["name"], "PASS")
+        passed += 1
+    else:
+        print(test["name"], "FAIL", "| expected:", expected, "got:", result)
+
+print(f"\n{passed}/{len(tests)} tests passed")

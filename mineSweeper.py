1925b5cf634452c6def2bdf7bc928dfcc94109c0
Lola Bug <139627294+lolabug11@users.noreply.github.com>
2026-06-09 01:06:59 +0000

    aaa

--- mineSweeper.py
+++ mineSweeper.py
@@ -11,13 +11,33 @@ board = [
     [0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0],
 ]
+flagBoard = [
+    [False,False,False,False,False,False,False,False,False],
+    [False,False,False,False,False,False,False,False,False],
+    [False,False,False,False,False,False,False,False,False],
+    [False,False,False,False,False,False,False,False,False],
+    [False,False,False,False,False,False,False,False,False],
+    [False,False,False,False,False,False,False,False,False],
+    [False,False,False,False,False,False,False,False,False],
+    [False,False,False,False,False,False,False,False,False],
+    [False,False,False,False,False,False,False,False,False],
+]
+def checkDirRepNums(changeR,changeC,r,c):
+    if r+changeR > -1 and c+changeC > -1 and r+changeR < 9 and c+changeC < 9:
+                    if board[r+changeR][c+changeC] == -1:
+                        board[r][c] += 1
+
+
+def checkDirShowNums(changeR,changeC,r,c):
+    pass
+
 def genBoard():
-    mine = randint(1,81)
+    mine = randint(1,25)
     mines = []
     mines.append(mine)
-    for _ in range(8):
+    for _ in range(9):
         mine = randint(1,81)
-        
+
         while mine in mines:
             mine = randint(1,81)
         mines.append(mine)
@@ -26,79 +46,42 @@ def genBoard():
         mineR = floor(mine / 9)
         mineC = mine % 9
         minesRC[mine] = [mineR, mineC]
+    print(minesRC)
     for mine in minesRC:
-        board[minesRC[mine][0]][minesRC[mine][1]] = -1
-    for r in range(8):
-        for c in range(8):
-            """
+        board[minesRC[mine][0]-1][minesRC[mine][1]-1] = -1
+    for r in range(9):
+        for c in range(9):
 
-             ox
+            if board[r][c] != -1:
+                changeR, changeC = 0,-1
+                checkDirRepNums(changeR,changeC,r,c)
 
-            """
-            changeR, changeC = 0,1
-            if r+changeR and c+changeC >=0:
-                if board[r+changeR][c+changeC] == -1:
-                    board[r][c] += 1
-            """
+                changeR, changeC = 0,1
+                checkDirRepNums(changeR,changeC,r,c)
+                
+                changeR, changeC = 1,1
+                checkDirRepNums(changeR,changeC,r,c)
+                
+                changeR, changeC = 1,0
+                checkDirRepNums(changeR,changeC,r,c)
+                
+                changeR, changeC = 1,-1
+                checkDirRepNums(changeR,changeC,r,c)
+            
+                changeR, changeC = -1,0
+                checkDirRepNums(changeR,changeC,r,c)
+                
+                changeR, changeC = -1,-1
+                checkDirRepNums(changeR,changeC,r,c)
+                
+                changeR, changeC = -1,1
+                checkDirRepNums(changeR,changeC,r,c)
 
-             o
-              x
-            """
-            changeR, changeC = 1,1
-            if r+changeR and c+changeC >=0:
-                if board[r+changeR][c+changeC] == -1:
-                    board[r][c] += 1
-            """
-
-             o
-             x
-            """
-            changeR, changeC = 1,0
-            if r+changeR and c+changeC >=0:
-                if board[r+changeR][c+changeC] == -1:
-                    board[r][c] += 1
-            """
-
-             o
-            x
-            """
-            changeR, changeC = 1,-1
-            if r+changeR and c+changeC >=0:
-                if board[r+changeR][c+changeC] == -1:
-                    board[r][c] += 1
-            """
-
-            xo
-
-            """
-            changeR, changeC = -1,0
-            if r+changeR and c+changeC >=0:
-                if board[r+changeR][c+changeC] == -1:
-                    board[r][c] += 1
-            """
-            x
-             o
-
-            """
-            changeR, changeC = -1,-1
-            if r+changeR and c+changeC >=0:
-                if board[r+changeR][c+changeC] == -1:
-                    board[r][c] += 1
-            """
-             x
-             o
-
-            """
-            changeR, changeC = -1,0
-            if r+changeR and c+changeC >=0:
-                if board[r+changeR][c+changeC] == -1:
-                    board[r][c] += 1
-            """
-              x
-             o
-
-            """
-            changeR, changeC = -1,1
-            if r+changeR and c+changeC >=0:
-                if board[r+changeR][c+changeC] == -1:
-                    board[r][c] += 1
+def openPlace(r,c):
+    chagn
+def click(r,c):
+    if not flagBoard[r][c]:
+        if board[r][c] == -1:
+            return -1
+        
+        
